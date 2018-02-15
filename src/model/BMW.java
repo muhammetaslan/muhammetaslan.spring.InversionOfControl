d65d11b3089b83e0596637e202d7e28042a21206
muhammetaslan <muhammet.aslan1995@gmail.com>
2018-02-16 00:10:32 +0300

    adding commiting

---
+++ src/model/BMW.java
@@ -0,0 +1,35 @@
+package model;
+
+public class BMW implements Car {
+
+	@Override
+	public void driveCar() {
+		
+		
+	}
+
+	@Override
+	public void stopCar() {
+		
+		
+	}
+
+	@Override
+	public void takePassenger(int numberOfPassenger) {
+		
+		
+	}
+
+	@Override
+	public void sellCar(int carId) {
+		
+		
+	}
+
+	@Override
+	public void buyCar(int carId) {
+		
+		
+	}
+
+}
---
+++ src/model/Volkswagen.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Volkswagen implements Car {
+
+	@Override
+	public void driveCar() {
+		
+		
+	}
+
+	@Override
+	public void stopCar() {
+		
+		
+	}
+
+	@Override
+	public void takePassenger(int numberOfPassenger) {
+		
+		
+	}
+
+	@Override
+	public void sellCar(int carId) {
+		
+		
+	}
+
+	@Override
+	public void buyCar(int carId) {
+		
+		
+	}
+}
